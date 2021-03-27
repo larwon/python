5d2e3b51d0f77e241c2daa1abaa497d62fb42cc5
larwon <74400003+larwon@users.noreply.github.com>
2021-03-27 21:25:36 +0900

    Add files via upload

--- Pandas_pt.py
+++ Pandas_pt.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 
-s = pd.Series([0, 0.25, 0.5, 0.75, 1.0])
-print(s)
-print(s.values)
-print(s.index)
-print(s[1])
-print(s[1:4])
+s = pd.Series([0, 0.25, 0.5, 0.75, 1],
+             index = ['a', 'b', 'c', 'd', 'e'])
+print(s) #index를 따로 구성
+print(s['c'])
+print(s[['c', 'd', 'e']])
+print('b') in s #s변수 안에 b가 있냐?
+
