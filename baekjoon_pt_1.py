49055b9ddf9a277361f71adfc06dd3466b819d98
larwon <74400003+larwon@users.noreply.github.com>
2021-04-09 18:58:34 +0900

    Add files via upload

---
+++ baekjoon_pt_1.py
@@ -0,0 +1,55 @@
+#2
+print("강한친구 대한육군\n강한친구 대한육군")
+print()
+print("강한친구 대한육군")
+print("강한친구 대한육군")
+
+
+#3
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
+
+#4
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"""\\")
+print("|\"^\"`    |")
+print("||_/=\\\__|")
+
+
+#5
+a = int(input("첫번째 숫자를 입력하시오 : "))
+b = int(input("두번째 숫자를 입력하시오 : "))
+
+print(a+b)
+
+
+#6
+a = int(input("숫자 x 를 입력하시오(1 <= x) :"))
+b = int(input("숫자 y 를 입력하시오(y <= 10,000) :"))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+
+#7
+#a = int(input("숫자 x 를 입력하시오 :"))
+#b = int(input("숫자 y 를 입력하시오 :"))
+a = input("숫자 x 를 입력하시오 :")
+b = input("숫자 y 를 입력하시오 :")
+
+list_1 = b[2]
+list_2 = b[1]
+list_3 = b[0]
+
+print("3번째 자리 수 : ", int(a)*int(list_1))
+print("4번째 자리 수 : ", int(a)*int(list_2))
+print("5번째 자리 수 : ", int(a)*int(list_3))
+print("6번째 자리 수 : ", int(a)*int(b))
+
