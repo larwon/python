fcb736f5dd0260d8b54e449b50f23abcf87223ff
larwon <74400003+larwon@users.noreply.github.com>
2021-04-21 16:27:35 +0900

    Add files via upload

---
+++ baekjoon_pt_3.py.py
@@ -0,0 +1,50 @@
+# 1.
+# a = int(input("숫자를 입력해주세요 : "))
+#
+# for i in range(1, 10):
+#     print(a, "*", i, "=", a*i)
+
+
+# 2.
+# a = int(input("첫번째 숫자를 입력해주세요 : "))
+# b = int(input("두번째 숫자를 입력해주세요 : "))
+#
+# print(a+b)
+
+
+# 3.
+# a = int(input("숫자를 입력해주세요 : "))
+#
+# x = 0
+# for i in range(1, a+1):
+#     x = x + i
+# print(x)
+
+
+# 4.
+# a = int(input("숫자를 입력해주세요 : "))
+#
+# x = 0
+# for i in range(1, a+1):
+#     print(i)
+
+
+# 5.
+# a = int(input("숫자를 입력해주세요 : "))
+#
+# for i in range(a, 0, -1):
+#     print(i)
+
+
+# 6.
+# a = int(input("숫자를 입력해주세요 : "))
+#
+# for i in range(1, a+1):
+#     print("*"*i)
+
+
+# 7.
+# a = int(input("숫자를 입력해주세요 : "))
+#
+# for i in range(1, a+1):
+#     print(" "*(a-i) + "*"*i)
