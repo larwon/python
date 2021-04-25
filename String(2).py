9a059c29e00498d116f482a85a28a0d57d8e0988
larwon <74400003+larwon@users.noreply.github.com>
2021-04-25 22:34:31 +0900

    Add files via upload

---
+++ String(2).py
@@ -0,0 +1,48 @@
+# 31.
+# a = "3"
+# b = "4"
+#
+# print(a+b)
+
+
+# 32.
+# print("HI" * 3)
+
+
+# 33.
+# print("-" * 80)
+
+
+# 34.
+# t1 = "python"
+# t2 = "java"
+# print((t1+" "+t2+" ") * 4)
+
+
+# 35.
+# name1 = "김민수"
+# age1 = 10
+# name2 = "이철희"
+# age2 = 13
+#
+# print("이름 : {0} 나이 : {1}".format(name1, age1))
+# print("이름 : {0} 나이 : {1}".format(name2, age2))
+# print(f"이름 : {name1} 나이 : {age1}") # f-spring
+
+
+# 36.
+# a = "5,969,782,550"
+# b = a.replace(",", "")
+# c = int(b)
+# print(c, type(c))
+
+
+# 37.
+# a = "2020/03(E) (IFRS연결)"
+# print(a[:7])
+
+
+# 38.
+# data = "   삼성전자   "
+# data1 = data.strip()
+# print(data1)
