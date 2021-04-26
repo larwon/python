170ba81292c9afc48f0bf964f34f869ba4a3121c
larwon <74400003+larwon@users.noreply.github.com>
2021-04-26 13:57:54 +0900

    Add files via upload

---
+++ String(1).py
@@ -0,0 +1,50 @@
+'''
+# 41.
+ticker = "btc_krw"
+print(ticker.upper())
+
+
+# 42.
+ticker = "btc_krw"
+print(ticker.lower())
+
+
+# 43.
+a = "hello"
+print(a.capitalize())
+
+
+# 44.
+a = "BlockDMask"
+print(a.swapcase())
+
+
+# 45.
+file_name = "보고서.xlsx"
+print(file_name.endswith(("xlsx", "xls")))
+
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith("2020"))
+
+
+# 46.
+a = "hello world"
+print(a.split())
+
+
+date = "2020-05-01"
+print(date.split("-"))
+
+
+# 47.
+data = "039490      "
+print(data.rstrip())
+
+
+
+# 48.
+a = "가나다라마바사"
+print(a.find('나'))
+print(a.find('다', 1))
+print(a.find('가', 3))
+'''
