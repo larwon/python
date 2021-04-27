920b6c9f16354222dec5b003918b45defa2f32e2
larwon <74400003+larwon@users.noreply.github.com>
2021-04-27 11:58:37 +0900

    Add files via upload

---
+++ list(2).py
@@ -0,0 +1,36 @@
+'''
+# 61.
+price = ['20210427', 100, 130, 140, 150, 160]
+print(price[1:])
+
+
+# 62.
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[::2])
+print(nums[1::2])
+print(nums[::-1])
+print(nums[5], nums[7])
+
+
+# 63.
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print(" ".join(interest))
+print("/".join(interest))
+print("\n".join(interest))
+
+
+# 64.
+string = "삼성전자/LG전자/Naver"
+interest = string.split("/")
+print(interest)
+
+
+
+# 65.
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()
+print(data)
+data.reverse()
+print(data)
+'''
+
