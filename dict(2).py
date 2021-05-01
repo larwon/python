9adc46b97cefc8879314408a9fd3b33eea33fe14
larwon <74400003+larwon@users.noreply.github.com>
2021-05-01 23:32:07 +0900

    Add files via upload

---
+++ dict(2).py
@@ -0,0 +1,32 @@
+# 91.
+inventory = {
+        "메로나" : [300, 20],
+        "비비빅" : [400, 3],
+        "죠스바" : [250, 100]
+    }
+print(inventory)
+
+
+# 92.
+print(inventory["죠스바"][0])
+
+
+# 93.
+print(inventory["메로나"][1])
+
+
+# 94.
+inventory["월드콘"] = [500, 7]
+print(inventory)
+
+
+# 95.
+icecream = {
+        "탱크보이" : 1200,
+        "폴라포" : 1200,
+        "빵빠레" : 1800,
+        "월드콘" : 1500,
+        "메로나" : 300
+    }
+print(icecream.keys())
+print(icecream.values())
