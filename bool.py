250f50b55e47984313255762ac335d122e7b510c
larwon <74400003+larwon@users.noreply.github.com>
2021-05-02 23:22:19 +0900

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,20 @@
+# 101.
+print(3 == 5)
+
+
+# 102.
+x = 4
+print(1 < x < 5)
+
+
+# 103.
+print((3 == 3) and (4 != 3))
+
+
+# 104.
+if 4 < 3:
+    print("Hello World")
+else:
+    print("Hi, there.")
+    
+
