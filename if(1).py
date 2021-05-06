6a70b35b9880b11dec3f48203735c17ae3710997
larwon <74400003+larwon@users.noreply.github.com>
2021-05-06 21:15:31 +0900

    Add files via upload

---
+++ if(1).py
@@ -0,0 +1,27 @@
+'''
+# 101.
+a = "안녕하세요"
+print(a * 2)
+
+
+# 102.
+a = int(input("숫자를 입력해주세요: "))
+print(a + 10)
+
+
+# 103.
+a = int(input("숫자를 입력해주세요 : "))
+if a % 2 == 0:
+    print("짝수")
+else:
+    print("홀수")
+'''
+
+# 104.
+a = int(input("숫자를 입력해주세요 : "))
+b = a + 20
+
+if b > 255:
+    print(255)
+else:
+    print(b)
