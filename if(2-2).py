7db63dd1029ba2ccf89e72403e3eac7924dda569
larwon <74400003+larwon@users.noreply.github.com>
2021-05-11 23:37:23 +0900

    Add files via upload

---
+++ if(2-2).py
@@ -0,0 +1,12 @@
+# 124.
+
+a = int(input("number1: "))
+b = int(input("number2: "))
+c = int(input("number3: "))
+
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+else:
+    print(c)
