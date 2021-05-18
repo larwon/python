eb35eaabba6ac048be6b42459bdc9f965aa78523
larwon <74400003+larwon@users.noreply.github.com>
2021-05-18 21:06:05 +0900

    Add files via upload

---
+++ for(1).py
@@ -0,0 +1,20 @@
+# 131.
+# fruit = ['사과', '귤', '바나나']
+# for i in fruit:
+#     print(i)
+
+# 132.
+# fruit = ['apple', 'banana', 'tomato']
+# for i in fruit:
+#     print("####")
+
+# 133.
+# number = [10, 20 ,30]
+# for i in number:
+#     print(i)
+
+# 134.
+# number = [10, 20, 30]
+# for i in number:
+#     print(i)
+#     print("---")
