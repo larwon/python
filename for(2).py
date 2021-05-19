eb62ae59e06e0406a099cc4187200ac8f58f277a
larwon <74400003+larwon@users.noreply.github.com>
2021-05-19 17:06:50 +0900

    Add files via upload

---
+++ for(2).py
@@ -0,0 +1,33 @@
+# 141.
+list = [100, 200, 300]
+
+for i in list:
+    print (i + 10)
+    
+
+
+# 142.
+list = ['김밥', '라면', '튀김']
+
+for i in list:
+    print("오늘의 메뉴: ", i)
+
+
+# 143.
+list = ['SK하이닉스', '삼성전사', 'LG전자']
+
+for i in list:
+    print(len(i))
+
+
+# 144.
+list = ['dog', 'cat', 'parrot']
+
+for i in list:
+    print(i, len(i))
+
+
+# 145.
+list = ['dog', 'cat', 'parrot']
+for i in list:
+    print(i[0])
