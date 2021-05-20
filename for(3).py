ea054df4833dff0ce9e8ca59a050a35f86249e21
larwon <74400003+larwon@users.noreply.github.com>
2021-05-20 16:29:11 +0900

    Add files via upload

---
+++ for(3).py
@@ -0,0 +1,31 @@
+# 145.
+list = [1, 2, 3]
+for i in list:
+    print("3 *", i)
+
+# 146.
+# 146-1.
+list = ['가', '나', '다', '라']
+list_a = list[1:]
+
+for i in list_a:
+    print(i)
+
+# 146-2.
+list = ['가', '나', '다', '라']
+for i in list[1:]:
+    print(i)
+
+
+# 147.
+list = ['가', '나', '다', '라']
+
+for i in list[::2]:
+    print(i)
+
+
+# 148.
+list = ['가', '나', '다', '라']
+
+for i in list[::-1]:
+    print(i)
