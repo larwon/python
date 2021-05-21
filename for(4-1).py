eb3f0c3b769e497669d78df897743feef08aec62
larwon <74400003+larwon@users.noreply.github.com>
2021-05-21 19:27:33 +0900

    Add files via upload

---
+++ for(4-1).py
@@ -0,0 +1,18 @@
+# 151.
+list = [3, -20, -3, 44]
+
+for i in list:
+    if i < 0:
+        print(i)
+    else:
+        pass
+
+
+# 152.
+list = [3, 100, 23, 44]
+
+for i in list:
+    if i % 3 == 0:
+        print(i)
+    else:
+        pass
