101d90b4e2f69a2d54efe12dd00b07ba578bd43c
larwon <74400003+larwon@users.noreply.github.com>
2021-05-22 15:13:40 +0900

    Add files via upload

---
+++ for(4-2).py
@@ -0,0 +1,17 @@
+# 153.
+list = [13, 21, 12, 14, 30, 18]
+for i in list:
+    if ((i < 20) and (i % 3 == 0)):
+        print(i)
+    else:
+        pass
+    
+
+# 154.
+list = ["I", "study", "python", "language", "!"]
+for i in list:
+    if len(i) > 2:
+        print(i)
+    else:
+        pass
+    
