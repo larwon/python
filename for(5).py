8084e40a1d722a47443afc0f109d49ced604f79f
larwon <74400003+larwon@users.noreply.github.com>
2021-05-25 15:11:40 +0900

    Add files via upload

---
+++ for(5).py
@@ -0,0 +1,16 @@
+# 161.
+for i in range(100):
+    print(i)
+
+
+# 162.
+for i in range(1,31):
+    if i % 3 == 0:
+        print(i)
+    else:
+        pass
+
+# 163.
+for i in range(100):
+    print(99 - i)
+    
