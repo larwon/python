e9bacbc24cf56ffbdcdbb5541556103bfdfb7711
larwon <74400003+larwon@users.noreply.github.com>
2021-05-26 18:28:14 +0900

    Add files via upload

---
+++ for(5-1).py
@@ -0,0 +1,13 @@
+# 165.
+for i in range(10):
+    print(i/10)
+
+
+# 166.
+for i in range(10):
+    print("3 x", i, "=", 3 * i)
+
+
+# 167.
+for i in range(1, 10, 2):
+    print("3 x", i, "=", 3 * i)
