3f586a3b460bb97b91a8fc78923c5d96aa6e5c34
larwon <74400003+larwon@users.noreply.github.com>
2021-05-27 11:21:37 +0900

    Add files via upload

---
+++ for(5-2).py
@@ -0,0 +1,25 @@
+# 168.
+sum = 0
+for i in range(1, 11):
+    sum = sum + i
+
+print(sum)
+
+
+# 169.
+sum = 0
+for i in range(1, 11):
+    if i % 2 != 0:
+        sum = sum + i
+    else:
+        pass
+
+print(sum)
+
+
+# 170.
+gop = 1
+for i in range(1, 11):
+   gop =  gop * i
+
+print(gop)
